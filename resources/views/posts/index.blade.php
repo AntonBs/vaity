1ef6173b94be8516bb4a5b7ed6a2a619c63330bd
AntonBs <reloded1995@gmail.com>
2020-08-06 04:53:28 +0300

    style for index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts/layout')
 @section('content')
-    <div>
+    <div class="container-post">
         @foreach ($posts as $post)
 
 
 
-            <div >{{$post->title}}</div>
+{{--            <div >{{$post->title}}</div>--}}
 {{--            <div >{!! $post->article !!} </div>--}}
-            <div class="post-img" style="background-image: url({{ $post->img }})"></div>
-{{--            <div class="post-index">--}}
-{{--                <div class="post-title"><h2>{{ $post->title }}</h2></div>--}}
-{{--                <div class="post-article">--}}
-{{--                    <div class="card-img" style="background-image: url({{ $post->img ?? asset('img/picca.jpg') }})"></div>--}}
+{{--            <div class="post-img" style="background-image: url({{ $post->img }})"></div>--}}
+            <div class="post-index">
+                <div class="post-title"><h2>{{ $post->title }}</h2></div>
+                <div class="post-article">
+                    <div class="post-img" style="background-image: url({{ $post->img }})"></div>
 {{--                    <div class="card-author">Автор: {{ $post->name }}</div>--}}
 {{--                    <a href="{{ route('post.show', $post) }}" class="btn btn-outline-primary">Посмотреть пост</a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+                </div>
+            </div>
 
 
 
